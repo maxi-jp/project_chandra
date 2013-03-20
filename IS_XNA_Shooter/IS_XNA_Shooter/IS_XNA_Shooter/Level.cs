58d9be490c9970ca26bd591268f15fd55ed05582
fermonas <fermonast@gmail.com>
2013-03-20 13:36:26 +0100

    Complete level with all the enemies

--- IS_XNA_Shooter/IS_XNA_Shooter/IS_XNA_Shooter/Level.cs
+++ IS_XNA_Shooter/IS_XNA_Shooter/IS_XNA_Shooter/Level.cs
@@ -82,7 +82,31 @@ namespace IS_XNA_Shooter
                             GRMng.frameWidthEB1, GRMng.frameHeightEB1, GRMng.numAnimsEB1, GRMng.frameCountEB1,
                             GRMng.loopingEB1, SuperGame.frameTime12, GRMng.textureEB1, time, 1000, 100,
                             4, null);
-
+                    else if (enemyType.Equals("enemyWeakShot"))
+                        enemy = new EnemyWeakShot(camera, this, new Vector2(positionX, positionY), 0,
+                            GRMng.frameWidthEW2, GRMng.frameHeightEW2, GRMng.numAnimsEW2, GRMng.frameCountEW2,
+                            GRMng.loopingEW2, SuperGame.frameTime12, GRMng.textureEW2, time, 100, 100,
+                            1, null);
+                    else if (enemyType.Equals("enemyMineShot"))
+                        enemy = new EnemyMineShot(camera, this, new Vector2(positionX, positionY), 0,
+                            GRMng.frameWidthEMS, GRMng.frameHeightEMS, GRMng.numAnimsEMS, GRMng.frameCountEMS,
+                            GRMng.loopingEMS, SuperGame.frameTime12, GRMng.textureEMS, time, 100, 100,
+                            1, null);
+                    else if (enemyType.Equals("enemyLaserA"))
+                        enemy = new EnemyLaserA(camera, this, new Vector2(positionX, positionY), 0,
+                            GRMng.frameWidthEL, GRMng.frameHeightEL, GRMng.numAnimsEL, GRMng.frameCountEL,
+                            GRMng.loopingEL, SuperGame.frameTime12, GRMng.textureEL, time, 100, 100,
+                            1, null);
+                    else if (enemyType.Equals("enemyScared"))
+                        enemy = new EnemyScared(camera, this, new Vector2(positionX, positionY), 0,
+                            GRMng.frameWidthES, GRMng.frameHeightES, GRMng.numAnimsES, GRMng.frameCountES,
+                            GRMng.loopingES, SuperGame.frameTime12, GRMng.textureES, time, 100, 100,
+                            1, null);
+                    else if (enemyType.Equals("enemyWeakB"))
+                        enemy = new EnemyWeakB(camera, this, new Vector2(positionX, positionY), 0,
+                            GRMng.frameWidthEW1, GRMng.frameHeightEW1, GRMng.numAnimsEW1, GRMng.frameCountEW1,
+                            GRMng.loopingEW1, SuperGame.frameTime12, GRMng.textureEW1, time, 100, 100,
+                            1, null);
                     enemies.Add(enemy);
                     
                 }
@@ -105,7 +129,7 @@ namespace IS_XNA_Shooter
             this.ship = ship;
             foreach (Enemy e in enemies)// enemigos
                 e.SetShip(ship);
-            ship.SetPosition(ShipInitPosition);
+             ship.SetPosition(ShipInitPosition);
         }
 
         // This method returns true when the level is finished.

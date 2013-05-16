1847ca8771f5efa42ded46a3f4755a1c42dba869
Daniel Cuesta Boluda <phoe88@gmail.com>
2013-05-16 04:26:51 -0700

    we have solve some problems with the region map to displace the level.

--- IS_XNA_Shooter/IS_XNA_Shooter/IS_XNA_Shooter/MapEditor/MainMapEditor.cs
+++ IS_XNA_Shooter/IS_XNA_Shooter/IS_XNA_Shooter/MapEditor/MainMapEditor.cs
@@ -25,9 +25,12 @@ namespace IS_XNA_Shooter.MapEditor
         private Vector2 displacementLevel;
         private Vector2 lastPositionMouse;
 
+        SpriteFont spriteDebug;
+
         
         
         public MainMapEditor(String levelType, int width, int height, SuperGame mainGame) {
+            spriteDebug = GRMng.fontText;
 
             this.levelType = levelType;
           /*  this.height = width;
@@ -58,6 +61,11 @@ namespace IS_XNA_Shooter.MapEditor
             {
                 displacementLevel.X += Mouse.GetState().X - lastPositionMouse.X;
                 displacementLevel.Y += Mouse.GetState().Y - lastPositionMouse.Y;
+                if (displacementLevel.X > 0) displacementLevel.X = 0;
+                if (displacementLevel.X < 1000 - width - 20) displacementLevel.X = 1000 - width - 20;
+
+                if (displacementLevel.Y > 0) displacementLevel.Y = 0;
+                if (displacementLevel.Y < 500 - height - 20) displacementLevel.Y = 500 - height - 20;
             }
             lastPositionMouse.X = Mouse.GetState().X;
             lastPositionMouse.Y = Mouse.GetState().Y;
@@ -65,26 +73,6 @@ namespace IS_XNA_Shooter.MapEditor
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            background.Draw(spriteBatch);
-            spriteBatch.Draw(GRMng.blackpixeltrans, new Rectangle(SuperGame.screenWidth / 20, SuperGame.screenHeight / 6, 1000, 500), Color.Black);
-            // grid del suelo
-            for (int i = SuperGame.screenWidth / 20 + 10 + (int)displacementLevel.X; i < width + SuperGame.screenWidth / 20 + 10 + (int)displacementLevel.X; i += textureCell.Width)
-                for (int j = SuperGame.screenHeight / 6 + 10 + (int)displacementLevel.Y; j < height + SuperGame.screenHeight / 6 + 10 + (int)displacementLevel.Y; j += textureCell.Height)
-                {   int widthSourceRectangle = 1000 + SuperGame.screenWidth / 20 - i;
-                    int heightSourceRectangle = 500 + SuperGame.screenHeight / 6 - j;
-                    if (widthSourceRectangle > 80) widthSourceRectangle = 80;
-                    if (heightSourceRectangle > 80) heightSourceRectangle = 80;
-                    if (widthSourceRectangle < 0) widthSourceRectangle = 0;
-                    if (heightSourceRectangle < 0) heightSourceRectangle = 0;
-                    spriteBatch.Draw(textureCell, new Vector2(i, j), new Rectangle(0, 0, widthSourceRectangle, heightSourceRectangle), Color.White);
-                }
-
-            drawFrameEnemies(spriteBatch);
-
-            drawFrameProperties(spriteBatch);
-                
-            
-
             int origXNatLeft = SuperGame.screenWidth / 20 + 10 + (int)displacementLevel.X;
             int origXNatRight = origXNatLeft + width;
             int origYNatUp = SuperGame.screenHeight / 6 + 10 + (int)displacementLevel.Y;
@@ -104,6 +92,38 @@ namespace IS_XNA_Shooter.MapEditor
             if (tamWidthExtra < 0) tamWidthExtra = 0;
             int tamHeightExtra = origYScreen - origYNatUp;
             if (tamHeightExtra < 0) tamHeightExtra = 0;
+
+            background.Draw(spriteBatch);
+            spriteBatch.Draw(GRMng.blackpixeltrans, new Rectangle(SuperGame.screenWidth / 20, SuperGame.screenHeight / 6, 1000, 500), Color.Black);
+            // grid del suelo
+            for (int i = origXNatLeft; i < width + origXNatLeft; i += textureCell.Width)
+                for (int j = origYNatUp; j < height + origYNatUp; j += textureCell.Height)
+                {
+                    int widthSourceRectangle = endXScreen - i;
+                    int heightSourceRectangle = endYScreen - j;
+
+                    if (i < origXScreen && i + 80 > origXScreen || j < origYScreen && j + 80 > origYScreen)
+                    {
+                        int a = (int)(i - displacementLevel.X) % 80;
+                        int b = (int)(j - displacementLevel.Y) % 80;
+                        spriteBatch.Draw(textureCell, new Vector2(i - displacementLevel.X, j - displacementLevel.Y), new Rectangle(a, b, widthSourceRectangle, heightSourceRectangle), Color.White);
+                    }
+                    else if (i <= origXScreen && i + 80 <= origXScreen || j <= origYScreen && j + 80 <= origYScreen) { }
+                    else
+                    {
+
+
+                        if (widthSourceRectangle > 80) widthSourceRectangle = 80;
+                        if (heightSourceRectangle > 80) heightSourceRectangle = 80;
+                        if (widthSourceRectangle < 0) widthSourceRectangle = 0;
+                        if (heightSourceRectangle < 0) heightSourceRectangle = 0;
+                        spriteBatch.Draw(textureCell, new Vector2(i, j), new Rectangle(0, 0, widthSourceRectangle, heightSourceRectangle), Color.White);
+                    }
+                } 
+
+            drawFrameEnemies(spriteBatch);
+
+            drawFrameProperties(spriteBatch);
 
             // linea de arriba:
             if ( -origYScreen + origYNatUp >= 0)
@@ -130,6 +150,8 @@ namespace IS_XNA_Shooter.MapEditor
                 spriteBatch.Draw(whitePixel, new Rectangle(origenXRealLeft, origenYRealUp,
                     1, Math.Min(endYScreen - origYNatUp - tamHeightExtra, height - tamHeightExtra)), 
                     null, Color.White, 0, Vector2.Zero, SpriteEffects.None, 0);
+
+            spriteBatch.DrawString(spriteDebug, "(" + displacementLevel.X + ", " + displacementLevel.Y + ")", Vector2.Zero, Color.White);
         }
 
         private void drawFrameEnemies(SpriteBatch spriteBatch)
